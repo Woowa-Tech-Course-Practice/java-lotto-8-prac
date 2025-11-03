bcd9e12ca9c9ad8698abe9c42a5927767f34896d
JUNE <jh010303@naver.com>
2025-11-03 20:23:05 +0900

    feat: LottoCommandService, LottoQueryService 서비스 계층 구현
    - LottoCommandService: 로또 구매, 당첨 번호 설정, 통계 계산 등 명령(command) 처리
    - LottoQueryService: 로또 수량, 당첨 통계, 수익률 등 조회(query) 처리

---
+++ src/main/java/lotto/service/LottoQueryService.java
@@ -0,0 +1,31 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.Lottos;
+import lotto.domain.WinningRecord;
+
+import java.util.List;
+import java.util.Map;
+
+public class LottoQueryService {
+    public int getLottoCount() {
+        LottoMachine lottoMachine = LottoMachine.getInstance();
+        return lottoMachine.getLottoAmount();
+    }
+
+    public List<Lotto> getLottos(){
+        Lottos lottos = Lottos.getInstance();
+        return lottos.getLottos();
+    }
+
+    public Map<Integer, Integer> getWinningStatics() {
+        WinningRecord winningRecord = WinningRecord.getInstance();
+        return winningRecord.getRecords();
+    }
+
+    public double getRate() {
+        LottoMachine lottoMachine = LottoMachine.getInstance();
+        return lottoMachine.getRate();
+    }
+}
