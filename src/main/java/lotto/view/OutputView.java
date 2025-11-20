944c47569ac3fc4b950dc83e467edc810daa389b
JUNE <jh010303@naver.com>
2025-11-21 01:45:56 +0900

    refactor: Map의 key 변경
    - Integer에서 RankReward로 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -24,11 +24,11 @@ public class OutputView {
 
     public void winStatistics(Map<RankReward,Integer> winners) {
         System.out.println(WINNER_STATISTICS.getMessage());
-        System.out.println(FIFTH_STATISTICS.getMessage(FIFTH_LOTTO_PRICE,winners.getOrDefault(FIFTH, LOTTO_NUMBER_COUNT_NONE)));
-        System.out.println(FOURTH_STATISTICS.getMessage(FOURTH_LOTTO_PRICE ,winners.getOrDefault(FOURTH, LOTTO_NUMBER_COUNT_NONE)));
-        System.out.println(THIRD_STATISTICS.getMessage(THIRD_LOTTO_PRICE, winners.getOrDefault(THIRD, LOTTO_NUMBER_COUNT_NONE)));
-        System.out.println(SECOND_STATISTICS.getMessage(SECOND_LOTTO_PRICE, winners.getOrDefault(SECOND, LOTTO_NUMBER_COUNT_NONE)));
-        System.out.println(FIRST_STATISTICS.getMessage(FIRST_LOTTO_PRICE, winners.getOrDefault(FIRST, LOTTO_NUMBER_COUNT_NONE)));
+        System.out.println(FIFTH_STATISTICS.getMessage(FIFTH_LOTTO_PRICE,winners.getOrDefault(RankReward.FIFTH, LOTTO_NUMBER_COUNT_NONE)));
+        System.out.println(FOURTH_STATISTICS.getMessage(FOURTH_LOTTO_PRICE ,winners.getOrDefault(RankReward.FOURTH, LOTTO_NUMBER_COUNT_NONE)));
+        System.out.println(THIRD_STATISTICS.getMessage(THIRD_LOTTO_PRICE, winners.getOrDefault(RankReward.THIRD, LOTTO_NUMBER_COUNT_NONE)));
+        System.out.println(SECOND_STATISTICS.getMessage(SECOND_LOTTO_PRICE, winners.getOrDefault(RankReward.SECOND, LOTTO_NUMBER_COUNT_NONE)));
+        System.out.println(FIRST_STATISTICS.getMessage(FIRST_LOTTO_PRICE, winners.getOrDefault(RankReward.FIRST, LOTTO_NUMBER_COUNT_NONE)));
     }
 
     public void rate(double rate) {
