edcc4a24b1219d03bacc0fa00026eb24572159ba
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-11-21 21:42:48 +0900

    hotfix: magic number 대신 기존 정의한 matchCount 활용으로 변경

--- src/main/java/lotto/domain/util/LottoCalculator.java
+++ src/main/java/lotto/domain/util/LottoCalculator.java
@@ -44,11 +44,11 @@ public class LottoCalculator {
 
             // TODO -> 준형 : Rank enum에 findByCount() 만들어서 호출하는 쪽으로 변경
             // TODO -> 현빈 : Rank enum 구조 변경(description 추가) 및 기존 정의한 matchCount 활용으로 변경
-            if (matchCount == 6) return Rank.FIRST;
-            if (matchCount == 5 && bonusMatch) return Rank.SECOND;
-            if (matchCount == 5) return Rank.THIRD;
-            if (matchCount == 4) return Rank.FOURTH;
-            if (matchCount == 3) return Rank.FIFTH;
+            if (matchCount == FIRST.matchCount) return Rank.FIRST;
+            if (matchCount == SECOND.matchCount && bonusMatch) return Rank.SECOND;
+            if (matchCount == THIRD.matchCount) return Rank.THIRD;
+            if (matchCount == FOURTH.matchCount) return Rank.FOURTH;
+            if (matchCount == FIFTH.matchCount) return Rank.FIFTH;
             return Rank.NONE;
         }
     }
